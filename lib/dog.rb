bc41b91233ee2ded3bb504ed9572b5621b497444
Meghannwalsh <meghann.mccool.walsh@gmail.com>
2019-07-17 11:26:13 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog 
+
+    @@all = []
+    
+    def initialize
+       @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def self.clear_all
+        @@all = []
+    end 
+
+    def self.print_all
+        puts @@all
+    end 
+
+    def self.new(name)
+        @name = name 
+       @@all << name 
+    end 
+
+    def name 
+        @name 
+    end 
+end 
+
